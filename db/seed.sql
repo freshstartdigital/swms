06e0beb0026b487de2b73b7a8a0c88d4c4586a96
Ryan Slater <ryanryanslater@gmail.com>
2024-01-16 19:58:16 +1000

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -71,7 +71,7 @@ CREATE TABLE subscription_plans
   stripe_product_id VARCHAR(255) NOT NULL,
   stripe_payment_link VARCHAR(255),
   name VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NUL,
+  description VARCHAR(255) NOT NULL,
   price INTEGER NOT NULL DEFAULT 0,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
