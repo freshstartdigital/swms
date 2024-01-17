b730560c35548332672e717efeffe89cf9a27625
Ryan Slater <ryanryanslater@gmail.com>
2024-01-17 10:40:35 +1000

    fixes

--- app/internal/models/billing.go
+++ app/internal/models/billing.go
@@ -1,14 +1,14 @@
 package models
 
 type SubscriptionPlans struct {
-	ID                int    `json:"id"`
-	StripeProductId   string `json:"stripe_product_id"`
-	StripePaymentLink string `json:"stripe_payment_link"`
-	Name              string `json:"name"`
-	Description       string `json:"description"`
-	Price             int    `json:"price"`
-	CreatedAt         string `json:"created_at"`
-	UpdatedAt         string `json:"updated_at"`
+	ID                int     `json:"id"`
+	StripeProductId   string  `json:"stripe_product_id"`
+	StripePaymentLink *string `json:"stripe_payment_link"`
+	Name              string  `json:"name"`
+	Description       string  `json:"description"`
+	Price             int     `json:"price"`
+	CreatedAt         string  `json:"created_at"`
+	UpdatedAt         string  `json:"updated_at"`
 }
 
 type Subscriptions struct {
